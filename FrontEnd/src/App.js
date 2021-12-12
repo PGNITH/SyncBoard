ebc6744cdc85d8ef8e079c2ecf825c848df718a1
PGNITH <198003@nith.ac.in>
2021-12-12 15:27:58 +0530

    added background

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./styles/App.css";
+import Logo from "./assets/logo.png";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<div
+				className="container"
+				style={{
+					display: "flex",
+					alignItems: "center",
+					justifyContent: "center",
+					height: "100%",
+					width: "100%",
+					position: "absolute",
+				}}
+			>
+				<div
+					className="inner"
+					style={{
+						marginTop: "10px",
+						borderRadius: "10px",
+						height: "95%",
+						width: "97%",
+						position: "absolute",
+						background: "#f9f9f9",
+					}}
+				>
+					<div
+						className="head"
+						style={{
+							borderRadius: "10px",
+							height: "8%",
+							backgroundColor: "#ffffff",
+						}}
+					>
+						<header>
+							<div className="header">
+								<img
+									src={Logo}
+									alt="Logo"
+									width="20"
+									style={{
+										padding: "5px",
+
+										borderRight: "0.05px hsla(0,0%,50%,0.4) solid",
+									}}
+								/>
+							</div>
+						</header>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default App;
